e3c23dd9b5e01dffda7122415038c2e9cd09c341
Krishna Murthi <krishnavavilala098@gmail.com>
2025-11-30 10:03:18 +0530

    feat: add Task model and migrations

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    due_date = models.DateField(null=True, blank=True)
+    importance = models.IntegerField(default=5)
+    estimated_hours = models.IntegerField(default=1)
+    dependencies = models.JSONField(default=list, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.id} - {self.title}"
